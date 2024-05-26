65eef27e0f475f5f6d2291b4a7de9dfe81a14733
billy cole <brcengr@brcengineering.com>
2024-05-26 11:59:06 -0700

    Initial Commit

---
+++ navigationview/navigationviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationviewApp.swift
+//  navigationview
+//
+//  Created by Billy Cole on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct navigationviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
