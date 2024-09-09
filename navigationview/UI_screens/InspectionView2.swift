6130e3b30cfbe4053035f5490796afb0d513c369
billy cole <brc.cole@gmail.com>
2024-09-09 11:36:52 -0700

    reference 9/9/2024

---
+++ navigationview/UI_screens/InspectionView2.swift
@@ -0,0 +1,18 @@
+//
+//  InspectionView2.swift
+//  navigationview
+//
+//  Created by Tim Fan on 8/20/24.
+//
+
+import SwiftUI
+
+struct InspectionView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InspectionView2()
+}
---
+++ ttstViews/listView.swift
@@ -0,0 +1,18 @@
+//
+//  listView.swift
+//  navigationview
+//
+//  Created by Tim Fan on 9/4/24.
+//
+
+import SwiftUI
+
+struct listView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listView()
+}
